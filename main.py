d963a8678ae3b1a6fa1e60c1fa6df7aad26b6c1a
2021339023 <155388561+2021339023@users.noreply.github.com>
2025-11-14 07:55:03 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import asyncio
 
 from dotenv import load_dotenv
 from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, cli, llm
-from livekit.agents.voice_assistant import VoiceAssistant
+#from livekit.agents.voice_assistant import VoiceAssistant
+from livekit.agents.processors.voice_assistant import VoiceAssistant
 from livekit.plugins import openai, silero
 from api import AssistantFnc
 
@@ -35,4 +36,5 @@ async def entrypoint(ctx: JobContext):
 
 
 if __name__ == "__main__":
+
     cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint))
